0dba8ac8aea5888bb43d4c4834295487f6bffe9b
hatimchharchhoda <hitparker123@gmail.com>
2024-09-29 23:39:00 +0530

    changes

---
+++ n3.js
@@ -0,0 +1,12 @@
+const names = require('./n2')
+const say = require('./n4')
+const object = require('./n5-objects')
+
+
+require('./n6-function')
+
+console.log(object)
+
+sayHi('heehaa')
+sayHi(names.hatim)
+sayHi(names.yash)
